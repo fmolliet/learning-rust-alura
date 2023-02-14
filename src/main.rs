5208b9d3373f9818c0e384a3fda3802c1d90bff1
fmolliet <fmolliet@hotmail.com>
2023-02-13 23:32:49 -0300

    feat: Adicionando exposição de modulos e sub pastas

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ mod aula1;
 mod aula2;
 mod aula3;
 mod aula4;
+mod services;
+use services::{service};
 
 fn main(){
     aula1::main();
     aula2::main();
     aula3::main();
     aula4::main();
+    service::jooj();
 }
